af9b6a811128b4f7df266df669e2f9d752baf8cb
Shojin Masuda <shomasd@gmail.com>
2022-04-02 11:00:22 +0900

    add compile_flags for mydvorak

--- keyboards/crkbd/keymaps/mydvorak/keymap.c
+++ keyboards/crkbd/keymaps/mydvorak/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, KC_LEFT, KC_DOWN,KC_RIGHT,  L_COMB,  R_COMB,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, XXXXXXX,    KC_SPC,     KC_ENT, MO(3), KC_LALT
+                                          KC_LGUI, XXXXXXX,  KC_SPC,     KC_ENT, MO(3), KC_LALT
                                       //`--------------------------'  `--------------------------'
   ),
 
